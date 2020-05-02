9bcc9b8462fced2df463994dd503a637eeaeeb5b
hosup choi <mwmw77@gmail.com>
2020-05-02 15:05:20 +0900

    circle timer

--- lib/main.dart
+++ lib/main.dart
@@ -1,47 +1,174 @@
-// Copyright 2018 The Flutter team. All rights reserved.
-// Use of this source code is governed by a BSD-style license that can be
-// found in the LICENSE file.
-
 import 'package:flutter/material.dart';
-import 'spinner.dart';
+import 'dart:math' as math;
 
-void main() => runApp(MyApp());
+void main() => runApp(MaterialApp(
+  home: MyApp(),
+  theme: ThemeData(
+    canvasColor: Colors.blueGrey,
+    iconTheme: IconThemeData(
+      color: Colors.white,
+    ),
+    accentColor: Colors.pinkAccent,
+    brightness: Brightness.dark,
+  ),
+));
 
-class MyApp extends StatelessWidget {
+class MyApp extends StatefulWidget {
+  @override
+  MyAppState createState() => MyAppState();
+}
+
+class MyAppState extends State<MyApp> with TickerProviderStateMixin {
+  AnimationController controller;
+
+  // bool isPlaying = false;
+
+  String get timerString {
+    Duration duration = controller.duration * controller.value;
+    return '${duration.inMinutes}:${(duration.inSeconds % 60).toString().padLeft(2, '0')}';
+  }
+
+  @override
+  void initState() {
+    super.initState();
+    controller = AnimationController(
+      vsync: this,
+      duration: Duration(seconds: 10),
+    );
+
+    // ..addStatusListener((status) {
+    //     if (controller.status == AnimationStatus.dismissed) {
+    //       setState(() => isPlaying = false);
+    //     }
+
+    //     print(status);
+    //   })
+  }
+
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
-      title: 'Tabata',
-      theme: new ThemeData(
-        primarySwatch: Colors.red,
+    ThemeData themeData = Theme.of(context);
+    return Scaffold(
+      body: Padding(
+        padding: EdgeInsets.all(8.0),
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.spaceBetween,
+          children: <Widget>[
+            Expanded(
+              child: Align(
+                alignment: FractionalOffset.center,
+                child: AspectRatio(
+                  aspectRatio: 1.0,
+                  child: Stack(
+                    children: <Widget>[
+                      Positioned.fill(
+                        child: AnimatedBuilder(
+                          animation: controller,
+                          builder: (BuildContext context, Widget child) {
+                            return CustomPaint(
+                                painter: TimerPainter(
+                                  animation: controller,
+                                  backgroundColor: Colors.white,
+                                  color: themeData.indicatorColor,
+                                ));
+                          },
+                        ),
+                      ),
+                      Align(
+                        alignment: FractionalOffset.center,
+                        child: Column(
+                          mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                          crossAxisAlignment: CrossAxisAlignment.center,
+                          children: <Widget>[
+                            Text(
+                              "Count Down",
+                              style: themeData.textTheme.subhead,
+                            ),
+                            AnimatedBuilder(
+                                animation: controller,
+                                builder: (BuildContext context, Widget child) {
+                                  return Text(
+                                    timerString,
+                                    style: themeData.textTheme.display4,
+                                  );
+                                }),
+                          ],
+                        ),
+                      ),
+                    ],
+                  ),
+                ),
+              ),
+            ),
+            Container(
+              margin: EdgeInsets.all(8.0),
+              child: Row(
+                mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                children: <Widget>[
+                  FloatingActionButton(
+                    child: AnimatedBuilder(
+                      animation: controller,
+                      builder: (BuildContext context, Widget child) {
+                        return Icon(controller.isAnimating
+                            ? Icons.pause
+                            : Icons.play_arrow);
+
+                        // Icon(isPlaying
+                        // ? Icons.pause
+                        // : Icons.play_arrow);
+                      },
+                    ),
+                    onPressed: () {
+                      // setState(() => isPlaying = !isPlaying);
+
+                      if (controller.isAnimating) {
+                        controller.stop(canceled: true);
+                      } else {
+                        controller.reverse(
+                            from: controller.value == 0.0
+                                ? 1.0
+                                : controller.value);
+                      }
+                    },
+                  )
+                ],
+              ),
+            )
+          ],
+        ),
       ),
-      home: new MyHomePage(title: 'Spinner test'),
     );
   }
 }
 
-class MyHomePage extends StatefulWidget{
+class TimerPainter extends CustomPainter {
+  TimerPainter({
+    this.animation,
+    this.backgroundColor,
+    this.color,
+  }) : super(repaint: animation);
 
-  MyHomePage({Key key, this.title}) : super(key: key);
-
-  final String title;
+  final Animation<double> animation;
+  final Color backgroundColor, color;
 
   @override
-  _MyHomePageState createState() => new _MyHomePageState();
+  void paint(Canvas canvas, Size size) {
+    Paint paint = Paint()
+      ..color = backgroundColor
+      ..strokeWidth = 5.0
+      ..strokeCap = StrokeCap.round
+      ..style = PaintingStyle.stroke;
 
+    canvas.drawCircle(size.center(Offset.zero), size.width / 2.0, paint);
+    paint.color = color;
+    double progress = (1.0 - animation.value) * 2 * math.pi;
+    canvas.drawArc(Offset.zero & size, math.pi * 1.5, -progress, false, paint);
+  }
 
-}
-
-class _MyHomePageState extends State<MyHomePage> {
   @override
-  Widget build(BuildContext context) {
-    return new Scaffold(
-      appBar: new AppBar(
-        title: new Text(widget.title),
-      ),
-      body: new Center(
-        child: Spinner()
-      )
-    );
+  bool shouldRepaint(TimerPainter old) {
+    return animation.value != old.animation.value ||
+        color != old.color ||
+        backgroundColor != old.backgroundColor;
   }
 }
